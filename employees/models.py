3ff16d6cbfad7a4935a0376edeeac16012466ce3
salehmmasri <salehalmasri494@yahoo.com>
2021-09-14 20:24:39 +0300

    adding models

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+   
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    email = models.EmailField(blank=False, unique=True)
+    department = models.ForeignKey(Department, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
